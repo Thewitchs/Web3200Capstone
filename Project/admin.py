fc88bca8c483009df88d4cd223aac1a42c19b1f2
Thewitchs <lillianbuck@mail.weber.edu>
2026-02-22 23:53:13 -0700

    Part Two

--- Project/admin.py
+++ Project/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Book, BookMetadata, Publisher, Genre
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    list_filter = ('author', 'pages')
+    search_fields = ('title', 'author', 'pages')
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(BookMetadata)
+admin.site.register(Publisher)
+admin.site.register(Genre)

c5379b39ac905dcc56bf95dc5cd12db3d0d1a2cb
Thewitchs <lillianbuck@mail.weber.edu>
2026-02-22 23:38:58 -0700

    Part One

--- Project/models.py
+++ Project/models.py
@@ -2,12 +2,35 @@ from django.db import models
 from django.utils.text import slugify
 from django.urls import reverse
 # Create your models here.
+
+
+#many to one
+class Publisher(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+#many to many
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+    
+
+
+
+
 class Book(models.Model):
     title = models.CharField(max_length=10000)
     author = models.CharField(max_length=3000)
     pages = models.IntegerField()
     dds = models.FloatField(null=True)
     slug = models.SlugField(default='', null=False, db_index=True )
+
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+    genre = models.ManyToManyField(Genre)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -20,3 +43,12 @@ class Book(models.Model):
     def __str__(self):
         return f"{self.title}"
     
+    #one to one
+class BookMetadata(models.Model):
+    book = models.OneToOneField(Book, on_delete=models.CASCADE, null=True)
+    isbn = models.CharField(max_length=13)
+    publication_date= models.DateField()
+    summary = models.TextField()
+
+    def __str__(self):
+        return f"Metadata for {self.book.title}"
--- Project/urls.py
+++ Project/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.landing_page_view, name="home"),
-    path("about/", views.about_view, name="about")
+    path("about/", views.about_view, name="about"),
+     path('books/<slug:slug>/', views.book_detail, name='book-detail'),
 
 
 

0f4ade3797d002a24001db1bee7a3ddb2ae9dd07
Thewitchs <lillianbuck@mail.weber.edu>
2026-04-19 22:13:38 -0600

    Render stuff3

--- Final/settings.py
+++ Final/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'project',
+    'Final',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
